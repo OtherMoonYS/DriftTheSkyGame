fd251df8a2975a867e74636ac74d28c07d00a21d
DEDSEC2006 <110157528+DEDSEC2006@users.noreply.github.com>
2022-07-31 19:59:00 +0300

    Volume control without saving

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -6,27 +6,40 @@ public class AudioController : MonoBehaviour
     public Sprite audioOn;
     public Sprite audioOff;
     public GameObject buttonAudio;
+    private Image buttonAudioImage;
+    private bool isActive;
 
     public Slider slider;
 
     public AudioClip clip;
     public AudioSource audioSrc;
 
+    private void Awake()
+    {
+        buttonAudioImage = buttonAudio.GetComponent<Image>();
+    }
     void Update()
     {
-        audioSrc.volume = slider.value;
-    }
-    public void OnOffAudio()
-    {
-        if (AudioListener.volume == 1)
+        if (isActive == false)
         {
-            AudioListener.volume = 0;
-            buttonAudio.GetComponent<Image>().sprite = audioOff;
+            audioSrc.volume = 0;
         }
         else
         {
-            AudioListener.volume = 1;
-            buttonAudio.GetComponent<Image>().sprite = audioOn;
+            audioSrc.volume = slider.value;
+        }
+    }
+    public void OnOffAudio()
+    {
+        if (isActive)
+        {
+            isActive = false;
+            buttonAudioImage.sprite = audioOff;
+        }
+        else
+        {
+            isActive = true;
+            buttonAudioImage.sprite = audioOn;
         }
     }
     public void PlaySound()
