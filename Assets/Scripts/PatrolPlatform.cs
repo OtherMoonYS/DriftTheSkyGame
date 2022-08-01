c2cec5df43b5f8e32269924f2ddef8d288f48296
DEDSEC2006 <110157528+DEDSEC2006@users.noreply.github.com>
2022-08-01 18:40:53 +0300

    Moving platform

---
+++ Assets/Scripts/PatrolPlatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPlatform : MonoBehaviour
+{
+    private Transform _transform;
+    public Transform[] points;
+    private int moveIndex;
+    public float patrolSpeed;
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        _transform.position = Vector2.MoveTowards(_transform.position, points[moveIndex].position, patrolSpeed * Time.deltaTime);
+        if (Vector2.Distance(_transform.position, points[moveIndex].position) < 0.2f && moveIndex == 0)       
+            moveIndex = 1;
+
+        if (Vector2.Distance(_transform.position, points[moveIndex].position) < 0.2f && moveIndex == 1)
+            moveIndex = 0;
+
+    }
+}
