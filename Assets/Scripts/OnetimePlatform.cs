6a9df546ec3bba56f39bc61888c45aea6f76d790
DEDSEC2006 <110157528+DEDSEC2006@users.noreply.github.com>
2022-08-04 22:14:37 +0300

    Unfinished one-time platforms

---
+++ Assets/Scripts/OnetimePlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnetimePlatform : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.GetComponent<Doodle>() != null && other.relativeVelocity.y < 0)
+        anim.SetTrigger("Disappearance");
+    }
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -3,22 +3,11 @@
 public class Platform : MonoBehaviour
 {
     public float forceJump = 7.5f;
-    public Transform player;
-    public Vector2 finishPos;
-
-    public enum PlatformType { Usuale, Bonus }
-    public PlatformType type;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.relativeVelocity.y < 0)
-        {
-            if (type == PlatformType.Bonus)
-            {
-                Vector2 playerPos = player.position;
-                finishPos = playerPos + Vector2.up * forceJump;
-            }            
-
+        {                
             Doodle.instance.DoodleRigid.velocity = Vector2.up * forceJump;
         }        
     }
