167ede81b53222d02033fefafc7ade94ddc7dc2d
DEDSEC2006 <110157528+DEDSEC2006@users.noreply.github.com>
2022-08-04 11:27:30 +0300

    Pause and coins

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,24 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
     public GameObject pausePanel;
     private bool isPause;
-   
+
+    public GameObject[] disableObj;
+    public Text accountText;
+    public Text coinsText;
+    private MeterCounter counter;
+    private CoinCollect coins;
+
+    private void Start()
+    {
+        counter = FindObjectOfType<MeterCounter>();
+        coins = FindObjectOfType<CoinCollect>();
+    }
+
     public void OnPause()
     {
         if (isPause)
         {
             isPause = false;
             pausePanel.SetActive(false);
+            foreach (GameObject obj in disableObj)
+            {
+                obj.SetActive(true);
+            }
             Time.timeScale = 1;
         }
         else
         {
             isPause = true;            
             pausePanel.SetActive(true);
+            accountText.text = $"Высота: {counter.account} m";
+            coinsText.text = $"Количество монет: {coins.coinCountInGame}";
+            foreach (GameObject obj in disableObj)
+            {
+                obj.SetActive(false);
+            }
             Time.timeScale = 0;
         }
     }
