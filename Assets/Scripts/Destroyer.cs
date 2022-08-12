5490deead9c45134a4169e6ce7874b5fb19162a2
DEDSEC2006 <110157528+DEDSEC2006@users.noreply.github.com>
2022-08-12 10:18:11 +0300

    Optimize unfinished

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -3,8 +3,37 @@ using UnityEngine;
 public class Destroyer : MonoBehaviour
 {
     public float lifeTime;
+    public enum DestroyType { Time, OnTrigger, Blank };
+    public DestroyType type;
+
+    private Transform _transform;
+
+    private void Awake()
+    {
+        if (type.Equals(DestroyType.Blank))
+            _transform = GetComponent<Transform>();
+    }
+
     void Update()
     {
-        Destroy(gameObject, lifeTime);
+        if(type == DestroyType.Time)
+            Destroy(gameObject, lifeTime);
+        else if (type.Equals(DestroyType.Blank))
+        {
+            if (_transform.childCount.Equals(0))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (type == DestroyType.OnTrigger)
+        {
+            if (other.gameObject.name == "DeadZoneForPlatform")
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
